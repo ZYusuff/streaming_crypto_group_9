6072637d4bbedbfc712579315b90d72dd8d151cb
Nirre23 <niclas.karlman@hotmail.com>
2025-02-14 14:15:30 +0100

    changes

---
+++ src/consumer.py
@@ -0,0 +1,8 @@
+from quixstreams import Application
+from quixstreams.sinks.community.postgresql import PostgreSQLSink
+from constants import (
+    POSTGRES_DBNAME,
+    POSTGRES_HOST,
+    POSTGRES_PASSWORD,
+    POSTGRES_PORT,
+    POSTGRES_USER,)
