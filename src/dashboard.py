bceb7e2f3fb34162ae8796ba8fc7239fee728df8
Ferothanes <freja.kindstrand@hotmail.com>
2025-02-17 12:01:51 +0100

    src

---
+++ src/dashboard.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from constants import (
+    POSTGRES_DBNAME,
+    POSTGRES_HOST,
+    POSTGRES_PASSWORD,
+    POSTGRES_PORT,
+    POSTGRES_USER,
+)
+from sqlalchemy import create_engine 
+import pandas as pd
+
+connection_string = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DBNAME}"
+
+engine = create_engine(connection_string) #connectar till databas 
+
+query = "SELECT * FROM Shiba_Inu;"
+
+#När man gör en connection till databasen behöver man komma ihåg att stänga ner den
+# with statement kommer automatiskt stängas när den är färdig! Gör samma med filer(open.)
+with engine.connect() as connect:
+    df = pd.read_sql(query, connect)
+
+st.markdown("# Shibu Inu data")
+st.markdown("## latest data")
+
+st.dataframe(df.tail())
+
+#Gör en webbapplication med hjälp av Streamlit!
+
---
+++ src/producer.py
@@ -0,0 +1,66 @@
+import time
+
+from quixstreams import Application
+from constants import COINMARKET_API
+
+from requests import Session
+import json 
+from pprint import pprint
+
+
+#funktion av GET request. GET request data från vår API
+def get_latest_coin_data(target_symbol = "SHIB"):
+
+    API_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+
+    target_symbol = "SHIB"
+
+    parameters = {
+        'symbol': target_symbol,
+        'convert':'USD'
+    }
+
+    headers = {
+    'Accepts': 'application/json', #Accepts: This header tells the server that the client expects a JSON response.
+    'X-CMC_PRO_API_KEY': COINMARKET_API, #X-CMC_PRO_API_KEY: This is an API key used to authenticate the request to the CoinMarketCap API. You must have a valid key to access the API.
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    #Making the API Request
+    response = session.get(API_URL, params=parameters) #GET request, ADD URL
+    return json.loads(response.text)["data"][target_symbol] #return to JSON
+
+    pprint(data)
+
+#Funktion för vår Application, startar app
+#Varje 10de sekund kommer while loopen köras för att se uppdateringar, OM man betalar för API:et
+def main():
+    app = Application(broker_address="localhost:9092", consumer_group="coin_group")
+    coins_topic = app.topic(name = "coins", value_serializer="json")
+    #PRODUCER
+    with app.get_producer() as producer:
+        while True:
+            coin_latest = get_latest_coin_data("SHIB")
+
+
+            kafka_message = coins_topic.serialize(
+                key=coin_latest["symbol"], value=coin_latest
+            )
+            
+            print(
+                f"Produce event with key = {kafka_message.key}, price = {coin_latest['quote']['USD']['price']}"
+            )
+
+            producer.produce(
+                topic=coins_topic.name, key = kafka_message.key, value = kafka_message.value
+            )
+
+            time.sleep(40) 
+
+
+if __name__ == '__main__':
+    # coin_data = get_latest_coin_data()
+    # pprint(coin_data)
+    main()
