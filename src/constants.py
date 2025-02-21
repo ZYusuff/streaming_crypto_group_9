83d14e517793aebf99a757e6ae67ee1d3a5e99d7
Nirre23 <niclas.karlman@hotmail.com>
2025-02-21 11:20:08 +0100

    Added better comments to help understand the code and removed unused varibles and unused pprint

--- src/constants.py
+++ src/constants.py
@@ -1,13 +1,15 @@
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from a .env file
 load_dotenv()
 
+# API key for accessing the CoinMarketCap API (stored securely in environment variables)
 COINMARKET_API = os.getenv("COINMARKET_API")
 
+# PostgreSQL database connection details (retrieved from environment variables)
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-
